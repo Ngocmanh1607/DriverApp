0af2a0599a4c0227a00434a8991d5713a9be730f
Ngocmanh1607 <ngocmanh160703@gmail.com>
2025-04-18 01:31:25 +0700

    update

--- src/api/socket.js
+++ src/api/socket.js
@@ -3,7 +3,10 @@ import {io} from 'socket.io-client';
 // const socket = io('https://lh30mlhb-3000.asse.devtunnels.ms', {
 //     autoConnect: false,
 // });
-const socket = io('http://192.168.55.45:3000', {
+// const socket = io('http://192.168.55.45:3000', {
+//   autoConnect: false,
+// });
+const socket = io('http://localhost:3000', {
   autoConnect: false,
 });
 export default socket;
